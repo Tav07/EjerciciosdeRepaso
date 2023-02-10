1321443aa7a621004e271729cc6c34fcdb0208ff
Tav07 <gmendez650@soyudemedellin.edu.co>
2023-02-10 00:46:52 -0500

    Ejercicio1 completo

---
+++ Ejercicios/Ejercicio1.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    nombre = input("Por favor ingrese su nombre: ")
+    edad = input("Por favor ingrese su edad: ")
+
+    print("Su nombre es: " + nombre)
+    print("Su edad es: " + edad)
