8b39c0f225985abb64f72d5c22596ff9d2b9ca3c
Tav07 <gmendez650@soyudemedellin.edu.co>
2023-02-10 01:17:07 -0500

    Ejercicio 13 completo

---
+++ Ejercicios/Ejercicio 13.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    suma = num1 + num2
+    resta = num1 - num2
+    producto = num1 * num2
+    cociente = num1 / num2
+
+    print("La suma de", num1, "y", num2, "es", suma)
+    print("La resta de", num1, "y", num2, "es", resta)
+    print("El producto de", num1, "y", num2, "es", producto)
+    print("El cociente de", num1, "y", num2, "es", cociente)
